1a42765e40554d5a6d37f115f3ffd4a874643fe0
Shivam Kumar <119096144+Shivam29k@users.noreply.github.com>
2024-03-23 23:25:28 +0530

    nexttttt

---
+++ week-14/Notes/next-app/app/(auth)/signin/page.tsx
@@ -0,0 +1,12 @@
+
+
+import { SigninComponent } from "@/components/signin";
+
+export default function Signin() {
+  return (
+    <div>
+      <SigninComponent/>
+    </div>
+  )
+}
+
--- week-14/Notes/next-app/components/signin.tsx
+++ week-14/Notes/next-app/components/signin.tsx
@@ -1,4 +1,6 @@
-export default function Signin() {
+"use client"
+
+export function SigninComponent() {
   return (
     <div className="h-screen flex justify-center flex-col">
       <div className="flex justify-center">
@@ -21,6 +23,7 @@ export default function Signin() {
                 placeholder="123456"
               />
               <button
+                onClick={() => {console.log("Sign in")}}
                 type="button"
                 className="mt-8 w-full text-white bg-gray-800 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2"
               >
